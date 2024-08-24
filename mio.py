4c2712276d8f9a12801df11c9ba3f6ca1a49e2a2
puntolng-org <puntolngorg@gmail.com>
2024-08-23 21:54:29 -0500

    Create mio.py
    Este es el primer documento en el cual me estaré basando para construir este modelo que reportará muchos beneficios a padres, hijos, docentes y público interesado en este trabajo.

---
+++ mio.py
@@ -0,0 +1,53 @@
+# Ser emprendedor requiere una idea con la cual cambiar al mundo, Punto
+
+'''
+Modelo Interactivo ...
+MIO es un lenguaje de programación para niños. Una alternativa para introducirles a la programación de manera divertida y accesible.
+
+Estas son ideas para impleentar:
+
+- Sintaxis simple y fácil de entender
+- Uso de imágenes o iconos en lugar de texto
+- Ejemplos y tutoriales interactivos
+- Apoyo para proyectos visuales como animaciones o juegos
+- Integración con herramientas y plataformas populares entre los niños
+
+Se abre el programa y un avatar se presenta y le pregunta el nombre. Acto seguido, dice por voz audible "Cuéntame un cuento". Debes comenzar con el icónico "había una vez". Por debajo, el programa va creando un archivo .py mientras va interpretando el cuento/proyecto
+La palabra clave o el comando de voz para iniciar es "Había una vez". Un avatar que guía a los niños a crear un cuento interactivo y, al mismo tiempo, les enseña a programar de manera sutil. La frase "Había una vez" es un elemento familiar para los niños 
+que les permite expresar su creatividad. Hasta el momento no hay sintaxis, no hay código sólo el inicio de una conversación.
+
+El programa irá interpretando el cuento y creando un archivo .py en tiempo real, utilizando los elementos del cuento como clases, atributos, métodos, etc. Es una forma de enseñar conceptos de programación basados en sustantivos/clases, adjetivos/atributos, verbos/métodos.
+
+Para la implementación, se utilizarán bloques de código predefinidos, un editor oculto generado por la conversación. El cuento avanza y el avatar va creando una representación gráfica del proyecto. Por ejemplo, el niño dice: "Había una vez un caballo/cerdo/pato/gallo" , cualquier animal de granja, 
+el programa crea una clase vacía para llenar después. Visualmente en pantalla se muestra un círculo que representa dicha clase. Así, con cada nuevo elemento agregado, el usuario/estudiante, va a confirmar cómo se aprecia lo que va tomando forma de manera gradual.
+La visualización del cuento como un proyecto de programación, provee un adecuado entorno de retroalimentación continua que incentiva al niño a continuar su relato.
+
+A medida que el niño sigue contando el cuento, el programa irá agregando atributos y métodos a las clases correspondientes. Por ejemplo, si el niño dice "El caballo era negro y tenía una crin larga", el programa agregaría atributos como "color" y "longitud_de_la_crin" a la clase caballo.
+
+La representación gráfica podría ser aún más interesante si se agregaran elementos visuales que reflejen los atributos y acciones del cuento. Por ejemplo, si el niño dice "el caballo corrió hacia la pradera", el programa puede agregar un vector que indique la dirección de la acción o una pequeña animación que muestre al caballo corriendo.
+
+Se utilizarán técnicas de reconocimiento de voz como los de los asistentes personales para capturar la narración del niño. Para esto el avatar va retroalimentando con preguntas como, ¿y qué pasó con el caballo negro? ¿Qué quieres que haga el caballo? y como respuesta a las preguntas, el programa genera los respectivos métodos. Así se introducen los conceptos en la interacción.
+En pocas palabras el avatar actúa como un guía interactivo. HAce preguntas que fomentan la creatividad y pensamiento crítico del niño. Con el "¿Qué pasó?" o "¿Qué quieres que haga?", el avatar está introduciendo conceptos de programación de manera natural y sutil.
+
+Imaginen a un niño haciéndo que el caballo de su cuento levante vuelo y acto seguido, la representación gráfica de un caballo con más forma, surca el espacio superior de la pantalla, por encima de la clase llamada prado o el objeto montaña. Para el niño es un cuento, pero para el programa, son instrucciones específicas sobre clases y sus métodos puestos en acción.
+
+Cuando queremos enseñar que los métodos es todo lo que la clase hace, a través de preguntas como "¿Qué quieres que haga el caballo?" limitando las opciones, a los casos más comunes y apegados a la realidad. El niño está aprendiendo a pensar en términos de acciones y comportamientos, lo que es fundamental en la programación orientada a objetos.
+Mediante un algoritmo que escuche, responda, proponga al niño para que confirme con un Si o un No y al mismo tiempo escriba el código que debe ser capaz de ejecutarse. Es decir un algoritmo que no solo sea capaz de interactuar con el niño, sino que también pueda generar código ejecutable a partir de la conversación. Es un gran reto, pero estamos aquí para hacerlo realidad.
+
+El algoritmo debería tener las siguientes características:
+
+1. Reconocimiento de voz o pulsaciones en los botones de opciones que se mostrarán en pantalla.
+2. Análisis de lenguaje natural para identificar sustantivos, adjetivos calificativos y adjetivos posesivos, junto con verbos que sean implementados como métodos.
+3. Generación de código a partir de la conversación, utilizando un lenguaje base de programación com python.
+4. Capacidad para proponer preguntas y sugerencias para fomentar la creatividad y la continuidad del proceso de creación.
+5. Retroalimentación constructiva con lenguaje positivo en las partes donde resulta frustrante para la mayoría de programadores al no entender lo que se debe cambiar en el código.
+6. Capacidad para ejecutar el código que se ha generado y mostrar los resultados en tiempo real.
+
+Posibles Recursos:
+
+Reconocimiento de voz: Google Cloud Speech-to-Text, Microsoft Azure Speech Services.
+Análisis de lenguaje natural: NLTK, spaCy, Stanford CoreNLP.
+Generación de código: templating engines como Jinja2 o Mustache.
+Lenguaje de programación: Python, JavaScript, Scratch o Blocksy.
+
+'''
